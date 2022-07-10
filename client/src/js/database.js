1317a321913ca330ec3edc64bdf5254e4be88942
Wren Sanchez <demisa00@gmail.com>
2022-07-09 23:27:33 -0700

    Working on database.js file.

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,33 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.error('putDb not implemented');
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+}
+
+
+// Logic for a method that gets all the content from the database
+export const getDb = async () => {
+  try {
+  //Connect to database
+  const jateDb = await openDB("jate", 1);
+  //Create transaction for the database
+  const tx = jateDb.transaction("jate", "readwrite");
+  //Open object store
+  const store = tx.objectStore("jate");
+  //Pull all data
+  const request = store.getAll();
+
+  //Log results and error catch
+  const result = await request;
+  console.log("result.value", result);
+  return result;
+  }
+  catch (err) {
+  console.error("getDb not implemented");
+  }
+}
+
 
 initdb();
