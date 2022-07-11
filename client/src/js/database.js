72e58e190a449b38836ba24ef198e9fee92f0e0b
Wren Sanchez <demisa00@gmail.com>
2022-07-11 16:55:51 -0700

    Get function and added .gitignore file.

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,16 +12,20 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
+// Logic for a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  console.error('putDb not implemented');
-
+  console.log('PUT to database');
+  const jateDb = await openDB("jate", 1);
+  const tx = jateDb.transaction("jate", "readwrite");
+  const store = tx.objectStore("jate");
+  const request = store.put({id: 1, value: content});
+  const result = await request;
+  console.log('Data saved to database', result.value);
 }
 
 
 // Logic for a method that gets all the content from the database
 export const getDb = async () => {
-  try {
   //Connect to database
   const jateDb = await openDB("jate", 1);
   //Create transaction for the database
@@ -31,15 +35,13 @@ export const getDb = async () => {
   //Pull all data
   const request = store.getAll();
 
-  //Log results and error catch
+  //Log results
   const result = await request;
-  console.log("result.value", result);
-  return result;
+  result
+    ? console.log('Data retrieved from the database', result.value)
+    : console.log('Data not found in the database');
+  return result?.value;
   }
-  catch (err) {
-  console.error("getDb not implemented");
-  }
-}
 
 
 initdb();
